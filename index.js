84ff7bf1f11ca986b14ea3ac614b74cd8751ce43
Pavanitha02 <kalasanivenkat764@gmail.com>
2025-11-14 16:27:39 +0530

    isPrime function

---
+++ index.js
@@ -0,0 +1,17 @@
+let num = 15; 
+let isPrime = true;
+if (num <= 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+}
+if (isPrime) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
